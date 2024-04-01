b606664ccbc59dd5bbc02783fd7aed0ae7c807f3
thee-falcon <omar.makran.10@gmail.com>
2024-04-01 01:27:22 +0000

    the callenge is done

--- Project6c/Project6c/ViewController.swift
+++ Project6c/Project6c/ViewController.swift
@@ -17,15 +17,14 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         // title.
-        title = "Shoppping List"
+        title = "Shopping List"
         // the add Button.
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForList))
         // the refresh Button.
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startShopping))
-          
     }
     
-    // when the user Pressed om Add Button, That code will working.
+    // when the user Pressed on Add Button, That code will work.
     @objc func promptForList() {
         let ac = UIAlertController(title: "Enter your Order", message: nil, preferredStyle: .alert)
         
@@ -51,7 +50,7 @@ class ViewController: UITableViewController {
         }
         
         shoppingList.insert(newItem, at: 0)
-        // reload the data, to show the last submition.
+        // reload the data, to show the last submission.
         tableView.reloadData()
     }
     
@@ -61,17 +60,75 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
     
-    // spicifies the number of rows.
+    // specifies the number of rows.
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         shoppingList.count
     }
     
-    // to show the result.
+    // to displays the items in the table view.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for:  indexPath)
         
         cell.textLabel?.text = shoppingList[indexPath.row]
+        
+        // Create and configure the remove button
+        let removeButton = UIButton(type: .system)
+        removeButton.setTitle("Remove", for: .normal)
+        removeButton.setTitleColor(.red, for: .normal) // Set button title color to red
+        removeButton.sizeToFit() // Adjust button size to fit the title
+        removeButton.addTarget(self, action: #selector(removeItem(_:)), for: .touchUpInside)
+        
+        // Set the button as the accessory view of the cell
+        cell.accessoryView = removeButton
+        
         return cell
     }
+    
+    @objc func removeItem(_ sender: UIButton) {
+        // Get the cell containing the button that was tapped
+        guard let cell = sender.superview as? UITableViewCell else {
+            return
+        }
+        
+        // Get the index path of the cell
+        guard let indexPath = tableView.indexPath(for: cell) else {
+            return
+        }
+        
+        // Remove the item from the shopping list array
+        shoppingList.remove(at: indexPath.row)
+        
+        // Reload the table view to reflect the changes
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Deselect the tapped row
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // Get the selected cell
+        guard let cell = tableView.cellForRow(at: indexPath) else {
+            return
+        }
+        
+        // Toggle the editing mode of the selected cell
+        cell.setEditing(!cell.isEditing, animated: true)
+    }
+
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        // Set the editing style for the cell to .delete
+        return .delete
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Remove the item from the shopping list array
+            shoppingList.remove(at: indexPath.row)
+            
+            // Delete the row from the table view
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
 }
+
 
